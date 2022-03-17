ecff835ec6efd5b322749f5299865ffd0758e93b
OldSociety <headmistrust@outlook.com>
2022-03-17 13:48:49 -0700

    comment

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,6 +5,7 @@
 
  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
+ //Trying to change something. :)
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
